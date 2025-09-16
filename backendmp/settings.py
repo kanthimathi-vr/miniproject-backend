b39425f7d2e83f298ffae0b870c4ccfc8022050e
kanthimathi-vr <mathi.v.r.1990@gmail.com>
2025-09-16 22:08:15 +0530

    Update settings.py with cors

--- backendmp/settings.py
+++ backendmp/settings.py
@@ -73,8 +73,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "https://miniproject-frontend-zeta.vercel.app/",
+    
 ]
 
 CORS_ALLOW_HEADERS = [
